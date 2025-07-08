d3ad430c407e54dfa1fc15ad9aa00f96bf594992
Geoseanlee <lee.zixi.george@outlook.com>
2025-07-08 22:05:06 +1000

    mutation assignment

--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import "errors"
 
-func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
-	// check if the sizes of the arrays are the same
-	if len(names) != len(phoneNumbers) {
-		return nil, errors.New("invalid sizes")
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	u, exists := users[name]
+	if !exists {
+		return false, errors.New("not found")
 	}
-
-	// create a map
-	userMap := make(map[string]user)
-
-	// iterate over the names and phone numbers
-	for i := 0; i < len(names); i++ {
-		userMap[names[i]] = user{names[i], phoneNumbers[i]}
+	if !u.scheduledForDeletion {
+		return false, nil
 	}
-
-	return userMap, nil
+	delete(users, name)
+	return true, nil
 }
 
 type user struct {
-	name        string
-	phoneNumber int
+	name                 string
+	number               int
+	scheduledForDeletion bool
 }
